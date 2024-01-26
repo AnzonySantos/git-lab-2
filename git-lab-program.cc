5d296707cf93f9c82c394f6909f83bfaa0a950dc
asantos <am209722@ohio.edu>
2024-01-26 16:15:17 -0500

    New C++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Anzony Martinez Santos
+ *        Date: 1/26/24
+ * Description: Display Hello Git!!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
